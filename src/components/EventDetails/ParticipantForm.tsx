e27b5bb4ab9c1190a9917ea0331fd59ad12e7f16
Kazushi Konosu <kazushi@konosu.me>
2020-03-07 02:19:43 +0900

    refactor ParticipantForm

--- src/components/EventDetails/ParticipantForm.tsx
+++ src/components/EventDetails/ParticipantForm.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-underscore-dangle */
 import { Checkbox, Form, Input, InputNumber, Modal, Radio, Select } from 'antd'
 import { FormComponentProps } from 'antd/lib/form'
 import * as React from 'react'
@@ -24,96 +26,99 @@ const formItemLayout = {
 }
 const label = new Label()
 const { Option } = Select
-class _ParticipantForm extends React.Component<Props> {
-	public render() {
-		const { visible, onCancel, onCreate, title } = this.props
-		const { getFieldDecorator } = this.props.form
-		return (
-			<Modal
-				visible={visible}
-				title={`${title} – 参加申請`}
-				okText="申請する"
-				cancelText="キャンセル"
-				onCancel={onCancel}
-				onOk={onCreate}
-			>
-				<Form layout="horizontal">
-					<Form.Item label={label.name} {...formItemLayout}>
-						{getFieldDecorator('name', {
-							rules: [
-								{
-									required: true,
-									message: '氏名の入力は必須です',
-								},
-							],
-						})(<Input />)}
-					</Form.Item>
-					<Form.Item label={label.affiliation} {...formItemLayout}>
-						{getFieldDecorator('affiliation', {
-							rules: [
-								{
-									required: true,
-									message: '所属の入力は必須です',
-								},
-							],
-						})(<Input />)}
-					</Form.Item>
-					<Form.Item label={label.year} {...formItemLayout}>
-						{getFieldDecorator('year', {
-							rules: [
-								{
-									required: true,
-									message: '学年の入力は必須です',
-								},
-							],
-						})(
-							<Select>
-								{label.year_list.map((item: string) => (
-									<Option value={item} key={item}>
-										{item}
-									</Option>
-								))}
-							</Select>
-						)}
-					</Form.Item>
-					<Form.Item label={label.age} {...formItemLayout}>
-						{getFieldDecorator('age', {
-							rules: [
-								{
-									required: true,
-									message: '年齢の入力は必須です',
-									max: 80,
-									min: 18,
-									type: 'number',
-								},
-							],
-						})(<InputNumber />)}
-					</Form.Item>
-					<Form.Item label={label.sex} {...formItemLayout}>
-						{getFieldDecorator('sex', {
-							rules: [
-								{
-									required: true,
-									message: '性別の入力は必須です',
-								},
-							],
-						})(
-							<Radio.Group>
-								<Radio value="M">M</Radio>
-								<Radio value="F">F</Radio>
-							</Radio.Group>
-						)}
-					</Form.Item>
-					<Form.Item label={label.can_drive} {...formItemLayout}>
-						{getFieldDecorator('can_drive')(<Checkbox>可能</Checkbox>)}
-					</Form.Item>
-					<Form.Item label={label.note} {...formItemLayout}>
-						{getFieldDecorator('note')(<Input.TextArea />)}
-					</Form.Item>
-				</Form>
-			</Modal>
-		)
-	}
+const _ParticipantForm = (props: Props): React.ReactElement<any> => {
+	const {
+		visible,
+		onCancel,
+		onCreate,
+		title,
+		form: { getFieldDecorator },
+	} = props
+	return (
+		<Modal
+			visible={visible}
+			title={`${title} – 参加申請`}
+			okText="申請する"
+			cancelText="キャンセル"
+			onCancel={onCancel}
+			onOk={onCreate}
+		>
+			<Form layout="horizontal">
+				<Form.Item label={label.name} {...formItemLayout}>
+					{getFieldDecorator('name', {
+						rules: [
+							{
+								required: true,
+								message: '氏名の入力は必須です',
+							},
+						],
+					})(<Input />)}
+				</Form.Item>
+				<Form.Item label={label.affiliation} {...formItemLayout}>
+					{getFieldDecorator('affiliation', {
+						rules: [
+							{
+								required: true,
+								message: '所属の入力は必須です',
+							},
+						],
+					})(<Input />)}
+				</Form.Item>
+				<Form.Item label={label.year} {...formItemLayout}>
+					{getFieldDecorator('year', {
+						rules: [
+							{
+								required: true,
+								message: '学年の入力は必須です',
+							},
+						],
+					})(
+						<Select>
+							{label.year_list.map((item: string) => (
+								<Option value={item} key={item}>
+									{item}
+								</Option>
+							))}
+						</Select>
+					)}
+				</Form.Item>
+				<Form.Item label={label.age} {...formItemLayout}>
+					{getFieldDecorator('age', {
+						rules: [
+							{
+								required: true,
+								message: '年齢の入力は必須です',
+								max: 80,
+								min: 18,
+								type: 'number',
+							},
+						],
+					})(<InputNumber />)}
+				</Form.Item>
+				<Form.Item label={label.sex} {...formItemLayout}>
+					{getFieldDecorator('sex', {
+						rules: [
+							{
+								required: true,
+								message: '性別の入力は必須です',
+							},
+						],
+					})(
+						<Radio.Group>
+							<Radio value="M">M</Radio>
+							<Radio value="F">F</Radio>
+						</Radio.Group>
+					)}
+				</Form.Item>
+				<Form.Item label={label.can_drive} {...formItemLayout}>
+					{getFieldDecorator('can_drive')(<Checkbox>可能</Checkbox>)}
+				</Form.Item>
+				<Form.Item label={label.note} {...formItemLayout}>
+					{getFieldDecorator('note')(<Input.TextArea />)}
+				</Form.Item>
+			</Form>
+		</Modal>
+	)
 }
 
 export const ParticipantForm = Form.create({
