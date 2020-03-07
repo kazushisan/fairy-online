330f4190f882da1ba39e484a51cfc6d62b2535e5
Kazushi Konosu <kazushi@konosu.me>
2020-03-07 23:15:59 +0900

    export form component directly

--- src/components/EventForm/EventForm.tsx
+++ src/components/EventForm/EventForm.tsx
@@ -25,6 +25,7 @@ const formItemLayout = {
 		xs: { span: 24 },
 	},
 }
+
 const tailFormItemLayout = {
 	wrapperCol: {
 		xs: {
@@ -37,81 +38,87 @@ const tailFormItemLayout = {
 		},
 	},
 }
-const _EventForm = (props: Props): React.ReactElement<any> => {
-	const { visible, title, onOk, onCancel, onDelete, form, loading } = props
-	const { getFieldDecorator } = form
-	const footer = [
-		<Button key="back" onClick={onCancel}>
-			キャンセル
-		</Button>,
-		<Button key="submit" type="primary" onClick={onOk} loading={loading.submit}>
-			{title ? '変更する' : '作成する'}
-		</Button>,
-	]
-	if (typeof onDelete === 'function') {
-		footer.splice(
-			1,
-			0,
+
+export const EventForm = Form.create()(
+	(props: Props): React.ReactElement<any> => {
+		const { visible, title, onOk, onCancel, onDelete, form, loading } = props
+		const { getFieldDecorator } = form
+		const footer = [
+			<Button key="back" onClick={onCancel}>
+				キャンセル
+			</Button>,
 			<Button
-				key="delete"
-				type="danger"
-				onClick={onDelete}
-				loading={loading.delete}
+				key="submit"
+				type="primary"
+				onClick={onOk}
+				loading={loading.submit}
 			>
-				イベントを削除
-			</Button>
+				{title ? '変更する' : '作成する'}
+			</Button>,
+		]
+		if (typeof onDelete === 'function') {
+			footer.splice(
+				1,
+				0,
+				<Button
+					key="delete"
+					type="danger"
+					onClick={onDelete}
+					loading={loading.delete}
+				>
+					イベントを削除
+				</Button>
+			)
+		}
+		return (
+			<Modal
+				visible={visible}
+				title={title || 'イベントを作成'}
+				onCancel={onCancel}
+				onOk={onOk}
+				footer={footer}
+			>
+				<Form>
+					<Form.Item label="タイトル" {...formItemLayout}>
+						{getFieldDecorator('title', {
+							rules: [
+								{
+									required: true,
+									message: 'タイトルを入力してください',
+								},
+							],
+						})(<Input />)}
+					</Form.Item>
+					<Form.Item label="説明" {...formItemLayout}>
+						{getFieldDecorator('description', {
+							rules: [
+								{
+									required: true,
+									message: '説明を入力してください',
+								},
+							],
+						})(<Input.TextArea />)}
+					</Form.Item>
+					<Form.Item label="日程" {...formItemLayout}>
+						{getFieldDecorator('range', {
+							rules: [
+								{
+									required: true,
+									message: '日程を入力してください',
+								},
+							],
+						})(<DatePicker.RangePicker />)}
+					</Form.Item>
+					<Form.Item label="参加申請締切" {...formItemLayout}>
+						{getFieldDecorator('due', {})(<DatePicker />)}
+					</Form.Item>
+					<Form.Item {...tailFormItemLayout}>
+						{getFieldDecorator('can_apply', {
+							valuePropName: 'checked',
+						})(<Checkbox>参加を受け付ける</Checkbox>)}
+					</Form.Item>
+				</Form>
+			</Modal>
 		)
 	}
-	return (
-		<Modal
-			visible={visible}
-			title={title || 'イベントを作成'}
-			onCancel={onCancel}
-			onOk={onOk}
-			footer={footer}
-		>
-			<Form>
-				<Form.Item label="タイトル" {...formItemLayout}>
-					{getFieldDecorator('title', {
-						rules: [
-							{
-								required: true,
-								message: 'タイトルを入力してください',
-							},
-						],
-					})(<Input />)}
-				</Form.Item>
-				<Form.Item label="説明" {...formItemLayout}>
-					{getFieldDecorator('description', {
-						rules: [
-							{
-								required: true,
-								message: '説明を入力してください',
-							},
-						],
-					})(<Input.TextArea />)}
-				</Form.Item>
-				<Form.Item label="日程" {...formItemLayout}>
-					{getFieldDecorator('range', {
-						rules: [
-							{
-								required: true,
-								message: '日程を入力してください',
-							},
-						],
-					})(<DatePicker.RangePicker />)}
-				</Form.Item>
-				<Form.Item label="参加申請締切" {...formItemLayout}>
-					{getFieldDecorator('due', {})(<DatePicker />)}
-				</Form.Item>
-				<Form.Item {...tailFormItemLayout}>
-					{getFieldDecorator('can_apply', {
-						valuePropName: 'checked',
-					})(<Checkbox>参加を受け付ける</Checkbox>)}
-				</Form.Item>
-			</Form>
-		</Modal>
-	)
-}
-
-export const EventForm = Form.create()(_EventForm) as any
+)
