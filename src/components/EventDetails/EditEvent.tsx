f57f48c4bd05d76c19de246607a89f290322b71a
Kazushi Konosu <kazushi@konosu.me>
2020-03-08 21:36:31 +0900

    refactor: remove store from EditEvent

--- src/components/EventDetails/EditEvent.tsx
+++ src/components/EventDetails/EditEvent.tsx
@@ -3,12 +3,10 @@ import { Button, message, Form } from 'antd'
 import { History } from 'history'
 import moment from 'moment'
 import { handleError } from '../../services/handleError'
-import { EventStore } from '../../stores/EventStore'
 import { EventForm } from '../EventForm/EventForm'
 
 type Props = {
 	history: History
-	eventStore: EventStore
 }
 
 type Loading = {
@@ -16,10 +14,7 @@ type Loading = {
 	delete: boolean
 }
 
-export const EditEvent = ({
-	eventStore,
-	history,
-}: Props): React.ReactElement<{}> => {
+export const EditEvent = ({ history }: Props): React.ReactElement<{}> => {
 	const [visible, setVisible] = useState<boolean>(false)
 	const [loading, setLoading] = useState<Loading>({
 		submit: false,
@@ -27,7 +22,6 @@ export const EditEvent = ({
 	})
 	const [form] = Form.useForm()
 
-	const { event } = eventStore
 	const handleOk = async () => {
 		await form.validateFields().then((values: any) => {
 			console.log(values)
@@ -40,31 +34,9 @@ export const EditEvent = ({
 
 	const handleDelete = () => {
 		setLoading({ ...loading, delete: true })
-		eventStore
-			.removeEvent(eventStore.event.id)
-			.then(() => {
-				message.success('削除しました')
-				setLoading({ ...loading, delete: false })
-				setVisible(false)
-			})
-			.catch(err => {
-				setLoading({ ...loading, delete: false })
-				handleError({ err, history })
-			})
 	}
 
 	const handleClick = () => {
-		form.setFieldsValue({
-			title: event.title,
-			description: event.description,
-			range: [moment(event.start), moment(event.end)],
-			can_apply: event.can_apply || false,
-		})
-		if (event.due) {
-			form.setFieldsValue({
-				due: moment(event.due),
-			})
-		}
 		setVisible(true)
 	}
 
@@ -80,7 +52,7 @@ export const EditEvent = ({
 				onOk={handleOk}
 				onDelete={handleDelete}
 				loading={loading}
-				title={event.title}
+				title="イベントを編集する"
 			/>
 		</div>
 	)
