5e234764a8be674ee986c2f284061b818e1d43b0
Kazushi Konosu <kazushi@konosu.me>
2020-03-08 20:49:29 +0900

    fix: change LoginForm to migrate to antd v4

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useCallback } from 'react'
 import { Button, Form, Input, message } from 'antd'
-import { LockOutlined } from '@ant-design/icons'
-import { FormComponentProps } from 'antd/lib/form'
+import { LockOutlined, UserOutlined } from '@ant-design/icons'
 import { History } from 'history'
 import { parse } from 'query-string'
 import { UserStore } from '../../stores/UserStore'
@@ -16,96 +15,92 @@ const style = {
 	},
 }
 
-type Props = FormComponentProps & {
+type Props = {
 	userStore: UserStore
 	history: History
 }
 
-export const LoginForm = Form.create<Props>()(
-	({ userStore, history, form }: Props): React.ReactElement<{}> => {
-		const [loading, setLoading] = useState<boolean>(false)
+export const LoginForm = ({
+	userStore,
+	history,
+}: Props): React.ReactElement<{}> => {
+	const [form] = Form.useForm()
+	const [loading, setLoading] = useState<boolean>(false)
 
-		const handleSubmit = useCallback(
-			async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
-				e.preventDefault()
-				await new Promise(resolve =>
-					form.validateFields(async (error, values) => {
-						if (!error) {
-							setLoading(true)
-							await userStore
-								.login(values)
-								.then(() => {
-									message.success('ログインしました')
-									const { redirect } = parse(history.location.search)
-									if (redirect) {
-										history.push(decodeURIComponent(redirect as string))
-									} else {
-										history.push('/~fairyski/main')
-									}
-								})
-								.catch(err => {
-									setLoading(false)
-									if (err.status === 400) {
-										message.error('ユーザ情報が正しくありません。')
-									} else {
-										const text = err.data.message || 'Unknown Error'
-										const { status } = err
-										message.error(`${status}: ${text}`)
-									}
-								})
-							resolve()
-						}
-					})
-				)
-			},
-			[userStore, history, form]
-		)
+	const onFinish = useCallback(
+		async values => {
+			console.log(values)
 
-		const { getFieldDecorator } = form
+			setLoading(true)
+			await userStore
+				.login(values)
+				.then(() => {
+					message.success('ログインしました')
+					const { redirect } = parse(history.location.search)
+					if (redirect) {
+						history.push(decodeURIComponent(redirect as string))
+					} else {
+						history.push('/~fairyski/main')
+					}
+				})
+				.catch(err => {
+					setLoading(false)
+					if (err.status === 400) {
+						message.error('ユーザ情報が正しくありません。')
+					} else {
+						const text = err.data.message || 'Unknown Error'
+						const { status } = err
+						message.error(`${status}: ${text}`)
+					}
+				})
+		},
+		[userStore, history, form]
+	)
 
-		return (
-			<Form onSubmit={handleSubmit} style={style.form}>
-				<Form.Item>
-					{getFieldDecorator('user', {
-						rules: [
-							{
-								required: true,
-								message: 'ユーザ名を入力してください',
-							},
-						],
-						initialValue: 'general',
-					})(
-						<Input
-							prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-							placeholder="ユーザ名"
-						/>
-					)}
-				</Form.Item>
-				<Form.Item>
-					{getFieldDecorator('password', {
-						rules: [
-							{
-								required: true,
-								message: 'パスワードを入力してください',
-							},
-						],
-					})(
-						<Input
-							prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-							type="password"
-						/>
-					)}
-				</Form.Item>
-				<Button
-					type="primary"
-					htmlType="submit"
-					style={style.loginButton}
-					loading={loading}
-					disabled={loading}
-				>
-					ログイン
-				</Button>
-			</Form>
-		)
-	}
-)
+	return (
+		<Form
+			onFinish={onFinish}
+			style={style.form}
+			form={form}
+			initialValues={{ user: 'general' }}
+		>
+			<Form.Item
+				name="user"
+				rules={[
+					{
+						required: true,
+						message: 'ユーザ名を入力してください',
+					},
+				]}
+			>
+				<Input
+					prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+					placeholder="ユーザ名"
+				/>
+			</Form.Item>
+			<Form.Item
+				name="password"
+				rules={[
+					{
+						required: true,
+						message: 'パスワードを入力してください',
+					},
+				]}
+			>
+				<Input
+					prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+					type="password"
+				/>
+			</Form.Item>
+			<Button
+				type="primary"
+				htmlType="submit"
+				style={style.loginButton}
+				loading={loading}
+				disabled={loading}
+			>
+				ログイン
+			</Button>
+		</Form>
+	)
+}
