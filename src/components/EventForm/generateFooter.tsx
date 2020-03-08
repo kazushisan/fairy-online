43b654a1a9e0bed2f3bb92e16a0056bfca4a5c89
Kazushi Konosu <kazushi@konosu.me>
2020-03-08 21:17:02 +0900

    refactor: extract the footer generator from EventForm

---
+++ src/components/EventForm/generateFooter.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Button } from 'antd'
+
+type Props = {
+	title?: string
+	onOk: () => void
+	onCancel: () => void
+	onDelete?: () => void
+	loading: {
+		submit: boolean
+		delete?: boolean
+	}
+}
+
+export const generateFooter = ({
+	title,
+	onOk,
+	onCancel,
+	onDelete,
+	loading,
+}: Props): React.ReactElement<{}>[] => {
+	const buttons = [
+		<Button key="back" onClick={onCancel}>
+			キャンセル
+		</Button>,
+		<Button key="submit" type="primary" onClick={onOk} loading={loading.submit}>
+			{title ? '変更する' : '作成する'}
+		</Button>,
+	]
+
+	if (typeof onDelete === 'function') {
+		buttons.splice(
+			1,
+			0,
+			<Button
+				key="delete"
+				type="danger"
+				onClick={onDelete}
+				loading={loading.delete}
+			>
+				イベントを削除
+			</Button>
+		)
+	}
+
+	return buttons
+}
