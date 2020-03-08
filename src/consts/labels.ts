e6d3b3eeee7eec84b889767dd80ad3bbee5864b6
Kazushi Konosu <kazushi@konosu.me>
2020-03-08 22:06:14 +0900

    reafactor: create consts directory to store constants

---
+++ src/consts/labels.ts
@@ -0,0 +1,21 @@
+export const addEvent = '追加する'
+
+export const editEvent = '変更する'
+
+export const removeEvent = 'イベントを削除する'
+
+export const addParticipant = '追加する'
+
+export const name = '氏名'
+
+export const year = '年次'
+
+export const affiliation = '所属'
+
+export const sex = '性別'
+
+export const age = '年齢'
+
+export const canDrive = '車出し'
+
+export const note = '備考'
---
+++ src/consts/options.ts
@@ -0,0 +1,3 @@
+// todo: create an api to get the list
+
+export const yearOptions = ['B1', 'B2', 'B3', 'B4', 'M1', 'M2', 'その他']
