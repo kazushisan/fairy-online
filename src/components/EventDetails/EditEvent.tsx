6b579dab52eb593373a1526fb872434e9841157b
Kazushi Konosu <kazushi@konosu.me>
2020-03-08 21:28:23 +0900

    fix: rewrite EditEvent to match antd v4

--- src/components/EventDetails/EditEvent.tsx
+++ src/components/EventDetails/EditEvent.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useRef } from 'react'
-import { Button, message } from 'antd'
+import { Button, message, Form } from 'antd'
 import { History } from 'history'
 import moment from 'moment'
 import { handleError } from '../../services/handleError'
 import { EventStore } from '../../stores/EventStore'
-import { EventForm, EventFormRef } from '../EventForm/EventForm'
+import { EventForm } from '../EventForm/EventForm'
 
 type Props = {
 	history: History
@@ -25,45 +25,20 @@ export const EditEvent = ({
 		submit: false,
 		delete: false,
 	})
-
-	const formRef = useRef<EventFormRef>(null)
+	const [form] = Form.useForm()
 
 	const { event } = eventStore
 	const handleOk = async () => {
-		if (!formRef.current) {
-			return
-		}
-
-		const { form } = formRef.current
-		await form.validateFields(async (err: any, values: any) => {
-			if (!err) {
-				setLoading({ ...loading, submit: true })
-				const editEvent = {
-					start: values.range[0].format('YYYY-MM-DD'),
-					end: values.range[1].format('YYYY-MM-DD'),
-					due: values.due ? values.due.format('YYYY-MM-DD') : '',
-					can_apply: values.can_apply,
-					title: values.title,
-					description: values.description,
-				}
-				await eventStore
-					.editEvent(editEvent)
-					.then(() => {
-						setVisible(false)
-						setLoading({ ...loading, submit: false })
-						message.success('変更しました')
-					})
-					.catch(error => {
-						setLoading({ ...loading, submit: false })
-						handleError({ err: error, history })
-					})
-			}
+		await form.validateFields().then((values: any) => {
+			console.log(values)
 		})
 	}
+
 	const handleCancel = () => {
 		setVisible(false)
 	}
-	const handleDelete = async () => {
+
+	const handleDelete = () => {
 		setLoading({ ...loading, delete: true })
 		eventStore
 			.removeEvent(eventStore.event.id)
@@ -79,18 +54,14 @@ export const EditEvent = ({
 	}
 
 	const handleClick = () => {
-		if (!formRef.current) {
-			return
-		}
-
-		formRef.current.form.setFieldsValue({
+		form.setFieldsValue({
 			title: event.title,
 			description: event.description,
 			range: [moment(event.start), moment(event.end)],
 			can_apply: event.can_apply || false,
 		})
 		if (event.due) {
-			formRef.current.form.setFieldsValue({
+			form.setFieldsValue({
 				due: moment(event.due),
 			})
 		}
@@ -103,11 +74,11 @@ export const EditEvent = ({
 				編集する
 			</Button>
 			<EventForm
+				form={form}
 				visible={visible}
 				onCancel={handleCancel}
 				onOk={handleOk}
 				onDelete={handleDelete}
-				wrappedComponentRef={formRef}
 				loading={loading}
 				title={event.title}
 			/>
