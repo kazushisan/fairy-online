6f3a1ed7756582117f898b36eb478fb16ec90e01
Kazushi Konosu <kazushi@konosu.me>
2020-03-14 21:37:33 +0900

    refactor: App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import * as React from 'react'
+import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './App.scss'
 import { Routes } from './Routes'
-import store from './ducks'
+import store from './store'
 
 export const App = (): React.ReactElement<{}> => {
 	return (
-		<div>
+		<>
 			<Provider store={store}>
 				<Router>
 					<Routes />
 				</Router>
 			</Provider>
-		</div>
+		</>
 	)
 }
