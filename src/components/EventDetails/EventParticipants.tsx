2825915102bab3c8742d8192d0765dd239f04fd2
Kazushi Konosu <kazushi@konosu.me>
2020-03-14 20:11:32 +0900

    refactor: update EventParticipants

--- src/components/EventDetails/EventParticipants.tsx
+++ src/components/EventDetails/EventParticipants.tsx
@@ -1,118 +1,115 @@
+import React, { useState } from 'react'
 import { Button, Popconfirm, Table, Form } from 'antd'
 import { History } from 'history'
-import { set } from 'mobx'
-import { observer } from 'mobx-react'
-import React, { useState, useRef } from 'react'
 import styled from 'styled-components'
-import { Label } from '../../entities/Label'
-import { Participant } from '../../entities/Participant'
+
 import { handleError } from '../../services/handleError'
-import { EventStore } from '../../stores/EventStore'
 import { ParticipantForm } from './ParticipantForm'
+import * as label from '../../consts/label'
+import { Event } from '../../types/Event'
+import { Participant } from '../../types/Participant'
 
 type Props = {
-	eventStore: EventStore
 	history: History
 	canDelete: boolean
+	event: Event
+	removeParticipant: (participantId: Participant['id']) => Promise<any>
 }
-
-const label = new Label()
 
 const ButtonWrap = styled.div`
 	margin: 16px 0 0 0;
 `
 
-export const EventParticipants = observer(
-	({ eventStore, history, canDelete }: Props): React.ReactElement<{}> => {
-		const [adding, setAdding] = useState<boolean>(false)
+export function EventParticipants({
+	history,
+	canDelete,
+	removeParticipant,
+	event,
+}: Props): React.ReactElement<{}> {
+	const [adding, setAdding] = useState<boolean>(false)
 
-		const [form] = Form.useForm()
+	const [form] = Form.useForm()
 
-		const { participants } = eventStore.event
-		const { add_participant } = eventStore
+	const { participants } = event
 
-		const handleDelete = async (id: string) => {
-			await eventStore
-				.removeParticipant(id)
-				.catch(err => handleError({ err, history }))
-		}
+	const handleDelete = (id: string) =>
+		removeParticipant(id).catch(err => handleError({ err, history }))
 
-		const columns: object[] = [
-			{
-				dataIndex: 'name',
-				title: label.name,
-			},
-			{
-				dataIndex: 'affiliation',
-				title: label.affiliation,
-			},
-			{
-				dataIndex: 'age',
-				title: label.age,
-			},
-			{
-				dataIndex: 'sex',
-				title: label.sex,
-			},
-			{
-				dataIndex: 'can_drive',
-				render: (can_drive: boolean) => (can_drive ? 'はい' : 'いいえ'),
-				title: label.can_drive,
-			},
-			{
-				dataIndex: 'note',
-				title: label.note,
-			},
-			{
-				dataIndex: 'operation',
-				render: (_: any, record: Participant) => (
-					<Popconfirm
-						title="本当に削除しますか？"
-						onConfirm={() => handleDelete(record.id)}
-					>
-						<span>削除</span>
-					</Popconfirm>
-				),
-				title: '操作',
-			},
-		]
-		if (!canDelete) {
-			columns.pop()
-		}
-
-		const handleCreate = () => form.validateFields().then(values => console.log(values))
-		const handleCancel = () => {
-			setAdding(false)
-		}
-
-		return (
-			<div>
-				<Table
-					dataSource={participants}
-					columns={columns}
-					rowKey={(row: Participant) => row.id}
-					pagination={false}
-					scroll={{ x: true }}
-					bordered
-				/>
-				{eventStore.event.can_apply && (
-					<div>
-						<ParticipantForm
-							form={form}
-							add_participant={add_participant}
-							onCreate={handleCreate}
-							onCancel={handleCancel}
-							visible={adding}
-							title={eventStore.event.title}
-						/>
-						<ButtonWrap>
-							<Button type="primary" onClick={(): void => setAdding(true)}>
-								参加申請
-							</Button>
-						</ButtonWrap>
-					</div>
-				)}
-			</div>
-		)
+	const columns: object[] = [
+		{
+			dataIndex: 'name',
+			title: label.name,
+		},
+		{
+			dataIndex: 'affiliation',
+			title: label.affiliation,
+		},
+		{
+			dataIndex: 'age',
+			title: label.age,
+		},
+		{
+			dataIndex: 'sex',
+			title: label.sex,
+		},
+		{
+			dataIndex: 'can_drive',
+			render: (can_drive: boolean) => (can_drive ? 'はい' : 'いいえ'),
+			title: label.canDrive,
+		},
+		{
+			dataIndex: 'note',
+			title: label.note,
+		},
+		{
+			dataIndex: 'operation',
+			render: (_: any, record: Participant) => (
+				<Popconfirm
+					title="本当に削除しますか？"
+					onConfirm={() => handleDelete(record.id)}
+				>
+					<span>削除</span>
+				</Popconfirm>
+			),
+			title: '操作',
+		},
+	]
+	if (!canDelete) {
+		columns.pop()
 	}
-)
+
+	const handleCreate = () =>
+		form.validateFields().then(values => console.log(values))
+	const handleCancel = () => {
+		setAdding(false)
+	}
+
+	return (
+		<div>
+			<Table
+				dataSource={participants}
+				columns={columns}
+				rowKey={(row: Participant) => row.id}
+				pagination={false}
+				scroll={{ x: true }}
+				bordered
+			/>
+			{event.canApply && (
+				<div>
+					<ParticipantForm
+						form={form}
+						onCreate={handleCreate}
+						onCancel={handleCancel}
+						visible={adding}
+						title={event.title}
+					/>
+					<ButtonWrap>
+						<Button type="primary" onClick={(): void => setAdding(true)}>
+							参加申請
+						</Button>
+					</ButtonWrap>
+				</div>
+			)}
+		</div>
+	)
+}
