6a84f9848874a2d9f8b7188548324bd4eb42a987
Kazushi Konosu <kazushi@konosu.me>
2020-03-16 00:02:30 +0900

    feat: create participant model

---
+++ app/Models/Participant.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Participant extends Model {
+    protected $table = 'participants';
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'can_drive' => 'boolean',
+    ];
+
+    /**
+     * Get the event that the participant belongs to.
+     */
+    public function post()
+    {
+        return $this->belongsTo('App\Event');
+    }
+}
