473319b2050b0249ed4cff428d94d3d997ec956a
Kazushi Konosu <kazushi@konosu.me>
2020-03-15 19:10:14 +0900

    feat: create events table migration

---
+++ database/migrations/2020_03_15_100626_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('start');
+            $table->date('end');
+            $table->date('description');
+            $table->date('can_apply');
+            $table->date('due');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
