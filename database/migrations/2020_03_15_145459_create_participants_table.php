322f9523b5f58b6d3f645cd3d75040306fa9e1ee
Kazushi Konosu <kazushi@konosu.me>
2020-03-16 00:00:45 +0900

    chore: add migration for participants

---
+++ database/migrations/2020_03_15_145459_create_participants_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->foreignId('event_id');
+            $table->string('affiliation');
+            $table->string('year');
+            $table->string('sex');
+            $table->smallInteger('age');
+            $table->boolean('can_drive');
+            $table->string('note');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
