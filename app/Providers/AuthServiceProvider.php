9cc66a023fc6a1b912b2ffea7e02d393c7d768e9
Kazushi Konosu <kazushi@konosu.me>
2020-03-22 01:01:41 +0900

    fix: fix auth service provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use \Firebase\JWT\JWT;
+use Illuminate\Support\Str;
 use Illuminate\Auth\GenericUser;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -32,10 +33,10 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-            $auth_header = $this->header('Authorization', '');
+            $auth_header = $request->header('Authorization', '');
 
             if (!Str::startsWith($auth_header, 'Bearer ')) {
-                abort(400, 'authorization bearer missing.');
+                abort(400, 'Authorization bearer missing.');
             }
 
             $token = JWT::decode(Str::substr($auth_header, 7), env('JWT_KEY'), array('HS256'));
