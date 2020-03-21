3f3dc9529eabffc7373e81d59190d60eace39ce6
Kazushi Konosu <kazushi@konosu.me>
2020-03-22 00:30:58 +0900

    feat: register admin_only middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -78,6 +78,7 @@ $app->configure('app');
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
+    'admin_only' => App\Http\Middleware\AdminOnlyMiddleware::class,
 ]);
 
 /*
